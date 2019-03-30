03970a889a235273eb2b2e39afde1025f06264ce
tclerico <timclerico@gmail.com>
2019-03-30 15:10:05 -0400

    Reply updates

--- TwitterBot/__init__.py
+++ TwitterBot/__init__.py
@@ -3,19 +3,6 @@ from secrets import *
 import os
 import random
 
-codes = []
-replied = []
-
-
-def fillReplied():
-    with open('replied.txt') as f:
-        for line in f:
-            temp = line
-            temp = temp.strip()
-            replied.append(temp)
-            if 'str' in line:
-                break
-
 
 def blacklist(tweet):
     file = open('/home/pi/Desktop/twitterBot/FreedomFries/TwitterBot/replied.txt', 'a')
@@ -28,90 +15,83 @@ def searchTweet(api, searchTerm):
     return searchResults
 
 
-def tweet(api, text):
-    twts = searchTweet(api, "\"@FreedomFries16\"")
-    t = ['@FreedomFries16',
-         'freedomfries16',
-         'Freedomfries16',
-         '@freedomFires16',
-         'fry',
-         'Free fries',
-         'fries',
-         'free fry',
-         'free mcdonalds']
-    for s in twts:
-        user = s.user
-        uid = s.id_str
-        tid = s.id_str
-        for i in t:
-            if i in s.text:
-                for reply in replied:
-                    if tid not in replied:
-                        blacklist(tid)
-                        if uid != "1108539513834536961": #our own id
-                            # print(s.text)
-                            sn = s.user.screen_name
-                            # print(sn)
-                            m = "@" + sn + " " + text + " Use this code for a free medium fry!"
-                            s = api.update_status(m, s.id)
-                            print(m)
-
-def rem_from_file(code):
-    file = open('/home/pi/Desktop/twitterBot/FreedomFries/TwitterBot/testfile.txt', 'w')
-    for i in range(len(codes)):
-        if (i == code):
-            temp = "do nothing"
-        else:
-            file.write(codes[i] + "\n")
-    file.close()
-
-
-def readCodes():
-    with open('/home/pi/Desktop/twitterBot/FreedomFries/TwitterBot/testfile.txt') as f:
+def read_codes(path):
+    #'/home/pi/Desktop/twitterBot/FreedomFries/TwitterBot/testfile.txt'
+    codes = []
+    with open(path) as f:
         for line in f:
             temp = line
             temp = temp.strip()
             codes.append(temp)
             if 'str' in line:
                 break
+    return codes
 
 
 def find_mentions(api):
     mentions = [status for status in tweepy.Cursor(api.search, q="@FreedomFries16", include_entities=True).items(10)]
     print(len(mentions))
 
-    print(mentions[0].user.screen_name)
+    print(mentions[0].id)
+
+def add_user_to_file(user_info, path):
+    with open(path, 'a') as f:
+        id = user_info[0]
+        name = user_info[1]
+        f.write(str(id)+","+name+"\n")
 
 
-def reply_to_mentions(api):
-    #TODO READ IN A LIST OF USERS THAT HAVE BEEN REPLIED TO ALREADY
+def write_codes_to_file(codes, path):
+    with open(path, 'w') as f:
+        for code in codes:
+            f.write(str(code)+"\n")
+
+
+def size(file):
+    return os.stat(file).st_size
+
+
+def reply_to_mentions(api, blacklist_path, code_path):
+    # Read in list of users that have already been tweeted at  form: [[user_id, screen_name],[....]]
+    blacklist = []
+    if (size(blacklist_path) > 0):
+        with open(blacklist_path) as f:
+            str = f.readline().strip()
+            print(str)
+            blacklist.append([int(str.split(",")[0]),str.split(",")[1]])
+
+    # loop through all mentions
+    codes = read_codes(code_path)
     mentions = [status for status in tweepy.Cursor(api.search, q="@FreedomFries16", include_entities=True).items(10)]
     for m in mentions:
+        tweet_id = m.id
         user = m.user
-        uid = user.id
-        uname = user.screen_name
+        user_info = [user.id, user.screen_name]
 
-        #Check that user hasn't already been tweeted at today here
-        #
-        #
+        # if user is not in blacklist and has tweeted media then reply
+        # To reply to only pictures use: m.entities.get('media') is not None
+        if (user_info not in blacklist):
+            # get random code
+            to_use = random.randint(0, len(codes))
+            code_to_send = codes[to_use]
+            # remove code from list
+            codes.remove(code_to_send)
+            # Reply to user
+            status = "@"+user_info[1]+" "+code_to_send+" Use this code for a free medium fry!"
+            api.update_status(status, in_reply_to_status_id = tweet_id)
+            # Append user to file
+            add_user_to_file(user_info, blacklist_path)
 
-        if (m.entities.get('media') is not None):
-            # Reply to user with code
-            continue
-
-
+    # Re-write codes to file
+    write_codes_to_file(codes, code_path)
 
 
 def main():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)  # create an API object
-    find_mentions(api)
-    # readCodes()
-    # fillReplied()
-    # code = random.randint(0, len(codes))
-    # tweet_code = codes[code]
-    # tweet(api, tweet_code)
-    # rem_from_file(code)
-    # print("finished")
+    blacklist_path = "/Users/timc/PycharmProjects/FreedomFries/blacklist.txt"
+    code_path = "/Users/timc/PycharmProjects/FreedomFries/TwitterBot/testfile.txt"
+    reply_to_mentions(api, blacklist_path, code_path)
+
 main()
